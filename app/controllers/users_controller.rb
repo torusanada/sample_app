9e3ec6c81b4f5b089cde444b891d2af7a21a0bfb
torusanada <sanada@happykreuz.com>
2015-02-20 02:12:15 +0000

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+ def create
+    @user = User.new(user_params)
     if @user.save
-       flash[:success] = "Welcome to the Sample App!"
-       redirect_to @user
-      # 保存の成功をここで扱う。
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
-  end
+ end
   
    private
 
